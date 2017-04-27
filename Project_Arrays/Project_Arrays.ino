a1d1b63e33e4915fbf176825efe99ac2d798a9c2
caitlin1530 <caitlin@beckertribe.com>
2017-04-27 16:54:09 +1000

    InitialArrays

---
+++ Project_Arrays/Project_Arrays.ino
@@ -0,0 +1,27 @@
+bool upperLevel[40][80];
+bool lowerLevel[80][40];
+bool rampLevel[12][40];
+
+void setup() {
+  // put your setup code here, to run once:
+  for(int i = 0; i < 40; i++){
+    for(int j = 0; j < 80; j++){
+      upperLevel[i][j] = 0;
+    }
+  }
+  for(int i = 0; i < 80; i++){
+    for(int j = 0; j < 40; j++){
+      lowerLevel[i][j] = 0;
+    }
+  } 
+  for(int i = 0; i < 12; i++){
+    for(int j = 0; j < 40; j++){
+      rampLevel[i][j] = 0;
+    }
+  } 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
